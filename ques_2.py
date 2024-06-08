74e0e375af73dcf67b4ac67d2030eb2db0759110
sanikakhanduja <138996276+sanikakhanduja@users.noreply.github.com>
2024-06-09 00:11:14 +0530

    ques_2

---
+++ ques_2.py
@@ -0,0 +1,6 @@
+#Write a python program that checks whether a given number is even or odd.
+a=int(input("please enter the number.."))
+if(a%2==0): #condition to check odd/even 
+    print("The number is even")
+else:
+    print("The number is odd")
---
+++ ques_3.py
@@ -0,0 +1,2 @@
+#Write a python program that calculates the factorial of a given number
+a=int(input("please enter the number..."))
