59026d898452a27775c6034c4fc4e35c05c91bb2
sanikakhanduja <138996276+sanikakhanduja@users.noreply.github.com>
2024-06-08 23:45:36 +0530

    ques1

---
+++ ques_1.py
@@ -0,0 +1,7 @@
+#Write a program that takes two numbers as input and prints their sum
+a=int(input("Please enter the first number ")) #taking input
+b=int(input("Please enter the second number ")) #taking input
+sum=a+b
+print("The sum of the two numbers is ",sum) #output
+
+
